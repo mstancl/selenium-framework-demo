642dde972b27eb3646028ef715ddc30a8c9164cf
je95959 <martin.stancl@kbc.com>
2022-10-26 16:44:29 +0200

    academy prep

--- src/test/java/com/mstancl/workshop/noFramework/NoFrameworkDemoClass.java
+++ src/test/java/com/mstancl/workshop/noFramework/NoFrameworkDemoClass.java
@@ -1,5 +1,7 @@
 package com.mstancl.workshop.noFramework;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class NoFrameworkDemoClass {
@@ -7,7 +9,11 @@ public class NoFrameworkDemoClass {
 
     @Test()
     public void noFrameworkTest() {
-
+        System.setProperty("webdriver.chrome.driver", "webdriver/chromedriver");
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://demoqa.com/");
+        chromeDriver.findElement(By.xpath(".//h5[text()='Elements']")).click();
+        chromeDriver.findElement(By.xpath(".//span[text()='Text Box']")).click();
     }
 
 
---
+++ src/test/java/com/mstancl/workshop/todo/WorkshopTODOTest.java
@@ -0,0 +1,15 @@
+package com.mstancl.workshop.todo;
+
+import com.mstancl.BaseSuite;
+import org.testng.annotations.Test;
+
+public class WorkshopTODOTest extends BaseSuite {
+
+
+    @Test(description = "TODO - Go to Elements - Radio button and test that the message shows what has been selected")
+    public void radioButtonTest() {
+
+    }
+
+
+}
